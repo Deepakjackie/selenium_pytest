7caa75cc45a63f8508a3e645d02ce539c82f2f64
Deepak <deepakjackie76@gmail.com>
2026-05-07 20:04:48 +0530

    fixed github actions

--- frameworks/conftest.py
+++ frameworks/conftest.py
@@ -1,17 +1,20 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+@pytest.fixture
+def browser():
 
+    chrome_options = Options()
 
-@pytest.fixture(scope="function")
-def driver():
-    service =Service(
-       ChromeDriverManager().install()
-    )
-    
-    driver= webdriver.Chrome(service=service)
-    driver.implicitly_wait(5)
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
     yield driver
+
     driver.quit()
