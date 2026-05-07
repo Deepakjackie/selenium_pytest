26d74ef12f68bc3e3613e72376d2d4b0c2344d71
Deepak <deepakjackie76@gmail.com>
2026-05-07 20:32:15 +0530

    fixed fixture issues

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
-def browser():
-
+def driver():
     chrome_options = Options()
-
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
 
     yield driver
-
     driver.quit()
+
+@pytest.fixture
+def browser(driver):
+    return driver
